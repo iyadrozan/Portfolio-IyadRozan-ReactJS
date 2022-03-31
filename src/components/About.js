322f02da0203b75435a90a54e62b100a3f5329d6
iyadrozan <iyadrozan26@gmail.com>
2022-03-31 14:05:59 +0800

    Modification About

--- src/components/About.js
+++ src/components/About.js
@@ -5,12 +5,18 @@ function About() {
     <>
       <section class="About">
         <section class="container">
-          <div class="container">
-            <div class="row g-2">
-              <div class="col-6">
-                <h4>Iyad Rozan Bone</h4>
-              </div>
-            </div>
+          <div class="title">
+            <h1>Iyad Rozan Bone</h1>
+          </div>
+          <div class="paragraf">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate est beatae nemo sequi. Error itaque repudiandae, sapiente velit possimus repellendus molestiae amet, ad ipsam suscipit magnam soluta voluptatibus
+              necessitatibus nemo nesciunt dolor quos officiis eius dignissimos cum? Illum modi unde aperiam ullam. Rerum ipsum quo incidunt sed quam eveniet nihil eligendi debitis dicta unde. Deserunt, ducimus ratione? Incidunt, obcaecati
+              ut quam quod laudantium nulla deleniti, quo dolor debitis consectetur quos! Eveniet unde cum doloribus iure quis! Reprehenderit, ex at earum ipsum, ipsa perspiciatis impedit veritatis repudiandae eaque dicta, fuga nisi
+              consequuntur sit in eveniet illum quod! Impedit delectus ipsa error, sint itaque mollitia repellat odio sunt, nemo, quisquam exercitationem voluptatibus quibusdam iure minima. Rerum, officiis numquam provident minima
+              temporibus, eos reiciendis repellat molestiae non laudantium ad illum rem facilis ex quam distinctio vero quisquam ullam consectetur? Ipsa natus ducimus qui illo totam necessitatibus voluptatum doloremque aut, cumque labore
+              consequatur similique?
+            </p>
           </div>
         </section>
       </section>
